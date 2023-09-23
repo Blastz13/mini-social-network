529b248938134959491dd5ecc4e5f5eeeff2e8dd
rom.sinitsyn <rom.sinitsyn@x5.ru>
2023-09-23 11:20:35 +0300

    feature: added business-logic for users

--- app/user/services/user.py
+++ app/user/services/user.py
@@ -21,15 +21,48 @@ class UserService:
         skip: int,
         limit: int = 12,
     ) -> list[User]:
-        pass
+        query = select(User).offset(skip).limit(limit)
+        result = session.execute(query)
+        return result.scalars().all()
 
     def create_user(
         self, email: str, password1: str, password2: str, nickname: str,
     ) -> User:
-        pass
+        if password1 != password2:
+            raise PasswordDoesNotMatchException
+
+        query = select(User).where(or_(User.email == email, User.nickname == nickname))
+        result = session.execute(query)
+        is_exist = result.scalars().first()
+        if is_exist:
+            raise DuplicateEmailOrNicknameException
+
+        user = User(email=email, password=password1, nickname=nickname)
+        session.add(user)
+        session.commit()
+        return user
 
     def is_admin(self, user_id: int) -> bool:
-        pass
+        result = session.execute(select(User).where(User.id == user_id))
+        user = result.scalars().first()
+        if not user:
+            return False
+
+        if user.is_admin is False:
+            return False
+
+        return True
 
     def login(self, email: str, password: str) -> LoginResponseSchema:
-        pass
+        result = session.execute(
+            select(User).where(and_(User.email == email, password == password)),
+        )
+        user = result.scalars().first()
+        if not user:
+            raise UserNotFoundException
+
+        response = LoginResponseSchema(
+            token=TokenHelper.encode(payload={'user_id': user.id}),
+            refresh_token=TokenHelper.encode(payload={'sub': 'refresh'}),
+        )
+        return response
